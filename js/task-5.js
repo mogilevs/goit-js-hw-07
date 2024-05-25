d732c5e0a869fa22e0be1929a5d4d3421f5d25a0
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-05-25 18:57:38 +0300

    do task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const colorText = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+changeColorBtn.addEventListener("click", bodyColor);
+
+function bodyColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  colorText.textContent = body.style.backgroundColor;
+}
+
