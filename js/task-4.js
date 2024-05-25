3a87f85d4f13425f52213652ed07dad1146934b8
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-05-25 14:05:35 +0300

    do task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
+const formElements = form.querySelectorAll("input");
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    const obj = {};
+    if (email === "" || password === "") { alert('All form fields must be filled in') } else {
+        obj[form.elements.email.name] = email.trim();
+        obj[form.elements.password.name] = password.trim();
+        console.log(obj);
+    };
+    
+    form.reset();
+});
 
